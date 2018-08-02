9ec974a2cb5e2c2a7a04c703f73aae42bb18f8bf
IsaacPilatuna <isapila1996@hotmail.com>
2018-08-02 14:56:34 -0500

    Clase Conexion Con String de Conexion

--- WindowsFormsApplication1/WindowsFormsApplication1/ModuloNotificaciones/CarpetaSolicitud/Solicitud.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/ModuloNotificaciones/CarpetaSolicitud/Solicitud.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using System.IO;
 using System.Windows.Forms;
 
-namespace Proyectov2.CarpetaSolicitud
+namespace WindowsFormsApplication1
 {
     class Solicitud
     {
