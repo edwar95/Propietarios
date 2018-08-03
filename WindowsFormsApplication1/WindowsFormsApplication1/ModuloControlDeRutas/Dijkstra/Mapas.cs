d7123e9576036afe86db8c3df2e9cb8748737581
ErickMora <erick_3069@hotmail.com>
2018-08-03 00:01:23 -0500

    Add files via upload

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/ModuloControlDeRutas/Dijkstra/Mapas.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Dijkstra
+{
+    class Mapas
+    {
+        //en este clase vamos a conectar con google maps.
+        
+    }
+}
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/ModuloControlDeRutas/Dijkstra/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DijkstraSimulator")]
+[assembly: AssemblyDescription("Simulador do Algoritmo de Dijkstra para grafos, com opção de exibição passo-a-passo.\r\n\r\nDesenvolvido em C#, para apresentação em seminário da disciplina SSC0630 - Inteligência Artificial, ministrada pelo Prof. Dr. João Luís Garcia Rosa, na Universidade de São Paulo, campus São Carlos.\r\n\r\nAutores:\r\n- Leonardo Beck Prates, Nº USP 7962121\r\n- Marlon Jordan Francisco, Nº USP 8668945\r\n- Mateus Ribeiro Vanzella, Nº USP 8504181\r\n- Vitor Bertozzi da Silva, Nº USP 8622401")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Universidade de São Paulo")]
+[assembly: AssemblyProduct("DijkstraSimulator")]
+[assembly: AssemblyCopyright("Copyright © 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d4dee5d2-7941-4407-9612-49a020725198")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.2.0.0")]
+[assembly: AssemblyFileVersion("2.2.0.0")]
+
+
